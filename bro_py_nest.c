a7b2081b40203bff1eb7159bf6c84597c5c3c73d
alhassan <121292207+akanni-fijab@users.noreply.github.com>
2025-06-12 14:13:20 +0100

    I sucessfully did the nested loop stuff

---
+++ bro_py_nest.c
@@ -0,0 +1,23 @@
+#include "stdio.h"
+
+int main(){
+    //      AIM ==> TO make that stuff brocode made in python
+
+   
+    
+    int i,j;
+   
+
+    for(i=0; i < 10; i++){
+        // printf("\n"); // print a new line after each iteration, does cause it to skip a line at 0,0
+
+        for(j=0; j<15; j++){
+            printf("#");
+        }
+        printf("\n");// enter a new line after eacch iteration 
+    }
+
+//sucess
+
+    return 0;
+}
