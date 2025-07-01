66de1551a194c102137d7b3c3f95266904ee346e
alhassan <121292207+akanni-fijab@users.noreply.github.com>
2025-07-01 10:40:30 +0100

    Update reverse_v2.c

--- reverse_v2.c
+++ reverse_v2.c
@@ -9,9 +9,6 @@ int main(){
     for(i=21;i>=0; i--){
         name_str[i]= getchar();
         printf("\n");
-        putchar(name_str[i]); //It's obtaining stdout and printing it out not obtaining from getchar()
-        // The next call of getchar() obtains the next char in the input stream 
-                               //and then stores it in at the required index of name_str[]
     }
     //bit pattern ==> content of a variable 
 
